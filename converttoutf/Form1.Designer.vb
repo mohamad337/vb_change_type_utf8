c1681fc59ec578535c93a5dc7b7233f7c5c5f137
mohamad337 <64073746+mohamad337@users.noreply.github.com>
2020-04-21 15:10:30 +0430

    upload v1.0
    Upload v1.0

---
+++ converttoutf/Form1.Designer.vb
@@ -0,0 +1,136 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.RadioButton1 = New System.Windows.Forms.RadioButton()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.ListBox1 = New System.Windows.Forms.ListBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(476, 224)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "Convert"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'RadioButton1
+        '
+        Me.RadioButton1.AutoSize = True
+        Me.RadioButton1.Location = New System.Drawing.Point(179, 230)
+        Me.RadioButton1.Name = "RadioButton1"
+        Me.RadioButton1.Size = New System.Drawing.Size(76, 17)
+        Me.RadioButton1.TabIndex = 1
+        Me.RadioButton1.TabStop = True
+        Me.RadioButton1.Text = "iner folders"
+        Me.RadioButton1.UseVisualStyleBackColor = True
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(12, 23)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(539, 20)
+        Me.TextBox1.TabIndex = 2
+        Me.TextBox1.Text = "D:\product\website\ediy\doc\html shop theme\00 rtl change\004\themeforest-6767902" & _
+            "-zeon-ecommerce-html-theme-v1.0\themeforest-6767902-zeon-ecommerce-html-theme-v1" & _
+            ".0\Zeon Documentation"
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(67, 229)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(68, 20)
+        Me.TextBox2.TabIndex = 3
+        Me.TextBox2.Text = "*.html"
+        '
+        'ListBox1
+        '
+        Me.ListBox1.FormattingEnabled = True
+        Me.ListBox1.Location = New System.Drawing.Point(12, 49)
+        Me.ListBox1.Name = "ListBox1"
+        Me.ListBox1.Size = New System.Drawing.Size(539, 160)
+        Me.ListBox1.TabIndex = 4
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(9, 7)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(37, 13)
+        Me.Label1.TabIndex = 5
+        Me.Label1.Text = "patch:"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(13, 233)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(34, 13)
+        Me.Label2.TabIndex = 6
+        Me.Label2.Text = "Type:"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(431, 7)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(120, 13)
+        Me.Label3.TabIndex = 7
+        Me.Label3.Text = "convert files type to utf8"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(563, 261)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.ListBox1)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.RadioButton1)
+        Me.Controls.Add(Me.Button1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents RadioButton1 As System.Windows.Forms.RadioButton
+    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
+    Friend WithEvents TextBox2 As System.Windows.Forms.TextBox
+    Friend WithEvents ListBox1 As System.Windows.Forms.ListBox
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+
+End Class
---
+++ converttoutf/Form1.vb
@@ -0,0 +1,61 @@
+﻿Imports System.IO
+
+Public Class Form1
+
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+        ListBox1.Items.Clear()
+        If RadioButton1.Checked = False Then
+            ChangeFileEncoding(TextBox1.Text, TextBox2.Text, IO.SearchOption.TopDirectoryOnly)
+            'For Each oFile In IO.Directory.GetFiles(TextBox1.Text, "*.*", IO.SearchOption.AllDirectories)
+            'IO.File.WriteAllText(oFile, IO.File.ReadAllText(oFile), System.Text.Encoding.UTF8)
+            'ListBox1.Items.Add("<br>Saved #:  - <i>Encoding ")
+            'Next
+        Else
+            Dim DirList As New ArrayList
+            GetDirectories(TextBox1.Text, DirList)
+            For Each direcc In DirList
+                ListBox1.Items.Add((direcc.ToString).Replace(TextBox1.Text, "/"))
+                ChangeFileEncoding(direcc.ToString, TextBox2.Text, IO.SearchOption.TopDirectoryOnly)
+            Next
+
+            ChangeFileEncoding(TextBox1.Text, TextBox2.Text, IO.SearchOption.TopDirectoryOnly)
+
+        End If
+    End Sub
+
+    Sub GetDirectories(ByVal StartPath As String, ByRef DirectoryList As ArrayList)
+        Dim Dirs() As String = Directory.GetDirectories(StartPath)
+        DirectoryList.AddRange(Dirs)
+        For Each Dir As String In Dirs
+            GetDirectories(Dir, DirectoryList)
+        Next
+        ' For Each item In DirectoryList
+        'ListBox1.Items.Add(item)
+        'Next
+    End Sub
+
+    Function ChangeFileEncoding(ByVal pPathFolder As String, ByVal pExtension As String, ByVal pDirOption As IO.SearchOption) As Integer
+
+        Dim Counter As Integer
+        Dim s As String
+        Dim reader As IO.StreamReader
+        Dim gEnc As System.Text.Encoding
+        Dim direc As IO.DirectoryInfo = New IO.DirectoryInfo(pPathFolder)
+        For Each fi As IO.FileInfo In direc.GetFiles(pExtension, pDirOption)
+            s = ""
+            reader = New IO.StreamReader(fi.FullName, System.Text.Encoding.Default, True)
+            s = reader.ReadToEnd
+            gEnc = reader.CurrentEncoding
+            reader.Close()
+
+            If (gEnc.EncodingName <> System.Text.Encoding.UTF8.EncodingName) Then
+                s = IO.File.ReadAllText(fi.FullName, gEnc)
+                IO.File.WriteAllText(fi.FullName, s, System.Text.Encoding.UTF8)
+                Counter += 1
+                ListBox1.Items.Add("<br>Saved #" & Counter & ": " & fi.FullName & " - <i>Encoding was: " & gEnc.EncodingName & "</i>")
+            End If
+        Next
+
+        Return Counter
+    End Function
+End Class
